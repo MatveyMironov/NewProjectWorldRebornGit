7048da621497d8007fffff69d43b77dc3989274a
MatveyMironov <matveymironof@gmail.com>
2026-01-22 12:49:49 +0300

    Storage progression

---
+++ NewProjectWorldReborn/Assets/_Source/Progression/Implementations/Rewards/Resource Reward/ResourceReward.cs
@@ -0,0 +1,32 @@
+using CustomInfoSystem;
+using ProgressionSystem.Quest.Testing;
+using ResourceSystem;
+using StorageSystem;
+using System;
+
+namespace ProgressionSystem.Quest.Implementations
+{
+    public class ResourceReward : IReward
+    {
+        private readonly IStorage _storage;
+
+        public ResourceReward(IStorage storage, IResourceDefinition resource, int amount, ResourceRewardDisplayerMB displayerPrefab)
+        {
+            _storage = storage ?? throw new ArgumentNullException(nameof(storage));
+            Resource = resource ?? throw new ArgumentNullException(nameof(resource));
+            Amount = amount;
+
+            Info = new ResourceRewardInfo(this, displayerPrefab);
+        }
+
+        public IResourceDefinition Resource { get; }
+        public int Amount { get; }
+
+        public ICustomInfo Info { get; }
+
+        public void Reward()
+        {
+            _storage.AddResource(Resource, Amount);
+        }
+    }
+}
---
+++ NewProjectWorldReborn/Assets/_Source/Progression/Implementations/Rewards/Resource Reward/ResourceRewardInfo.cs
@@ -0,0 +1,27 @@
+using CustomInfoSystem;
+using ProgressionSystem.Quest.Implementations;
+using System;
+using UnityEngine;
+
+namespace ProgressionSystem.Quest.Testing
+{
+    public class ResourceRewardInfo : ICustomInfo
+    {
+        private readonly ResourceReward _reward;
+        private readonly ResourceRewardDisplayerMB _displayerPrefab;
+
+        public ResourceRewardInfo(ResourceReward reward, ResourceRewardDisplayerMB displayerPrefab)
+        {
+            _reward = reward ?? throw new ArgumentNullException(nameof(reward));
+            _displayerPrefab = displayerPrefab != null ? displayerPrefab : throw new ArgumentNullException(nameof(displayerPrefab));
+        }
+
+        public GameObject CreateInfoObject()
+        {
+            ResourceRewardDisplayerMB displayer = GameObject.Instantiate(_displayerPrefab);
+            displayer.DisplayReward(_reward);
+
+            return displayer.gameObject;
+        }
+    }
+}
