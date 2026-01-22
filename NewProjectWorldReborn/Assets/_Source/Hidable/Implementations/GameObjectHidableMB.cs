322a2a7a269e948f6790362275b77a9d393a795e
MatveyMironov <matveymironof@gmail.com>
2026-01-22 15:22:33 +0300

    Hidable

---
+++ NewProjectWorldReborn/Assets/_Source/Hidable/Implementations/GameObjectHidableMB.cs
@@ -0,0 +1,15 @@
+﻿namespace HidableSystem.Implementations
+{
+    public class GameObjectHidableMB : AHidableMB
+    {
+        public override void Hide()
+        {
+            gameObject.SetActive(false);
+        }
+
+        public override void Show()
+        {
+            gameObject.SetActive(true);
+        }
+    }
+}
