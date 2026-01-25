f0d2fa9a90615ac5dfc4622acf11a862d467dd8d
MatveyMironov <matveymironof@gmail.com>
2026-01-25 14:32:56 +0300

    Unity event switch update

--- NewProjectWorldReborn/Assets/_Source/Switch/UnityEventSwitchMB.cs
+++ NewProjectWorldReborn/Assets/_Source/Switch/UnityEventSwitchMB.cs
@@ -30,6 +30,8 @@ namespace SwitchSystem
 
         public override bool TrySwitchTo(int stateIndex)
         {
+            if (stateIndex == CurrentStateIndex) return false;
+
             if (stateIndex >= 0 && stateIndex < States.Length)
             {
                 States[stateIndex].Invoke();
