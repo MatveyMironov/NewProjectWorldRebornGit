910a973af550071de67bc2774b773d5dece8d52e
MatveyMironov <matveymironof@gmail.com>
2026-02-28 11:40:27 +0300

    Manufacture building demolition registration

--- NewProjectWorldReborn/Assets/_Source/Building/Implementations/Building Configurations/Manufacture Building/Manufacture Building Recorder/AManufactureBuildingRegisterMB.cs
+++ NewProjectWorldReborn/Assets/_Source/Building/Implementations/Building Configurations/Manufacture Building/Manufacture Building Recorder/AManufactureBuildingRegisterMB.cs
@@ -3,23 +3,31 @@ using UnityEngine;
 
 namespace BuildingSystem.Implementations
 {
-    public abstract class AManufactureBuildingRecorderMB : MonoBehaviour
+    public abstract class AManufactureBuildingRegisterMB : MonoBehaviour
     {
         protected abstract IBuildingManufacturesManager BuildingManufacturesManager { get; }
+        protected abstract IStructureBuildingsManager StructureBuildingsManager { get; }
 
         protected virtual void OnEnable()
         {
             ManufactureBuildingConfigurationSO.OnManufactureBuildingCreated += RegisterManufactureBuilding;
+            StructureBuildingsManager.OnBuildingRemoved += UnregisterManufactureBuilding;
         }
 
         protected virtual void OnDisable()
         {
             ManufactureBuildingConfigurationSO.OnManufactureBuildingCreated -= RegisterManufactureBuilding;
+            StructureBuildingsManager.OnBuildingRemoved -= UnregisterManufactureBuilding;
         }
 
         private void RegisterManufactureBuilding(Building building, IManufacture manufacture)
         {
             BuildingManufacturesManager.TryAddBuildingManufacture(building, manufacture);
+        }
+
+        private void UnregisterManufactureBuilding(Building building)
+        {
+            BuildingManufacturesManager.TryRemoveBuildingManufacture(building);
         }
     }
 }
