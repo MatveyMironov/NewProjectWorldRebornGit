f780bfe327c8adafe0228541688c41e86f06ad11
MatveyMironov <matveymironof@gmail.com>
2026-02-28 12:53:21 +0300

    UI update

--- NewProjectWorldReborn/Assets/_Source/Service/Service/Service Provider/Service Providers Manager/IServiceProvidersManager.cs
+++ NewProjectWorldReborn/Assets/_Source/Service/Service/Service Provider/Service Providers Manager/IServiceProvidersManager.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ServiceSystem
 {
     public interface IServiceProvidersManager
     {
+        HashSet<ServiceProvider> Providers { get; }
+
         event Action<ServiceProvider> OnServiceProviderAdded;
         event Action<ServiceProvider> OnServiceProviderRemoved;
 
