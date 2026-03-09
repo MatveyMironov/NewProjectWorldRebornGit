907faadc479f65a24db436492a9885ca2fb04ee0
MatveyMironov <matveymironof@gmail.com>
2026-03-09 21:05:35 +0300

    Fix

--- NewProjectWorldReborn/Assets/_Source/Construction/Construction Resources Placing/ConstructionResourcesPlacingState.cs
+++ NewProjectWorldReborn/Assets/_Source/Construction/Construction Resources Placing/ConstructionResourcesPlacingState.cs
@@ -76,12 +76,12 @@ namespace ConstructionResourcesPlacingSystem
 
         public void FinishAction(Vector2Int cell)
         {
-            if (_constructionGridManager.CheckIfCanPlaceBuilding(cell, _buildingLayout.OccupiedCells)
+            if (_constructionGridManager.CheckIfCanPlaceLayoutAt(cell, _buildingLayout.OccupiedCells)
                 && TryConsumeResources())
             {
                 BuildingStructure structure = _buildingViewInstantiator.CreateBuildingStructure(_constructionConfiguration, cell, _buildingOrientation);
 
-                if (_constructionGridManager.TryPlaceBuilding(structure, cell))
+                if (_constructionGridManager.TryPlaceStructureAt(structure, cell))
                 {
                     _preview.HidePreview();
 
@@ -168,7 +168,7 @@ namespace ConstructionResourcesPlacingSystem
 
         private void ShowPlacementValidity(Vector2Int originCell)
         {
-            if (_constructionGridManager.CheckIfCanPlaceBuilding(originCell, _buildingLayout.OccupiedCells))
+            if (_constructionGridManager.CheckIfCanPlaceLayoutAt(originCell, _buildingLayout.OccupiedCells))
             {
                 _preview.ShowValidPlacement();
             }
