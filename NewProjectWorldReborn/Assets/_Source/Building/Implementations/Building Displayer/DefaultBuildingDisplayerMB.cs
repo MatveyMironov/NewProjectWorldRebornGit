e57071951cf734844df74611b67384823218f0b0
MatveyMironov <matveymironof@gmail.com>
2026-03-11 12:24:52 +0300

    Default building displayer

--- NewProjectWorldReborn/Assets/_Source/Building/Implementations/Building Displayer/DefaultBuildingDisplayerMB.cs
+++ NewProjectWorldReborn/Assets/_Source/Building/Implementations/Building Displayer/DefaultBuildingDisplayerMB.cs
@@ -3,28 +3,18 @@ using UnityEngine;
 
 namespace BuildingSystem.Implementations
 {
-    public class BuildingDisplayerMB : ABuildingDisplayerMB
+    public class DefaultBuildingDisplayerMB : ABuildingDisplayerMB
     {
         [SerializeField] private ABuildingInfoDisplayerMB infoDisplayer;
 
-        private Building _displayedBuilding;
-
         public override void DisplayBuilding(Building building)
         {
-            Clear();
-
-            //building.Structure.BuildingView.ShowSelection(true);
             infoDisplayer.DisplayBuildingInfo(building.Info);
-            _displayedBuilding = building;
         }
 
         public override void Clear()
         {
-            if (_displayedBuilding == null) return;
-
-            //_selectedBuilding.Structure.View.ShowSelection(false);
             infoDisplayer.Clear();
-            _displayedBuilding = null;
         }
     }
 }
