62703f2907b78c59511938ccf9688f74bea15d2c
MatveyMironov <matveymironof@gmail.com>
2026-03-14 20:30:10 +0300

    Building Manufactures Manager Update

--- NewProjectWorldReborn/Assets/_Source/Building/Implementations/Building Configurations/Manufacture Building/Building Manufactures Manager/IBuildingManufacturesManager.cs
+++ NewProjectWorldReborn/Assets/_Source/Building/Implementations/Building Configurations/Manufacture Building/Building Manufactures Manager/IBuildingManufacturesManager.cs
@@ -1,10 +1,17 @@
 ﻿using ManufactureSystem;
+using System;
 
 namespace BuildingSystem.Implementations
 {
     public interface IBuildingManufacturesManager
     {
+        Building[] Buildings { get; }
+
+        event Action<Building> OnBuildingAdded;
+        event Action<Building> OnBuildingRemoved;
+
         bool TryAddBuildingManufacture(Building building, IManufacture manufacture);
         bool TryRemoveBuildingManufacture(Building building);
+        bool TryGetBuildingManufacture(Building building, out IManufacture manufacture);
     }
 }
