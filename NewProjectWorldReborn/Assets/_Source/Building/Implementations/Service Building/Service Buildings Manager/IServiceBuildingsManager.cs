10a3d5182fa17b21744ed30498303632913875f4
MatveyMironov <matveymironof@gmail.com>
2026-03-14 19:42:05 +0300

    Building service menu

--- NewProjectWorldReborn/Assets/_Source/Building/Implementations/Service Building/Service Buildings Manager/IServiceBuildingsManager.cs
+++ NewProjectWorldReborn/Assets/_Source/Building/Implementations/Service Building/Service Buildings Manager/IServiceBuildingsManager.cs
@@ -1,10 +1,17 @@
 ﻿using ServiceSystem;
+using System;
 
 namespace BuildingSystem.Implementations
 {
     public interface IServiceBuildingsManager
     {
+        Building[] Buildings { get; }
+
+        event Action<Building> OnBuildingAdded;
+        event Action<Building> OnBuildingRemoved;
+
         bool TryAddServiceBuilding(Building building, ServiceProvider serviceProvider);
         bool TryRemoveServiceBuilding(Building building);
+        bool TryGetBuildingServiceProvider(Building building, out ServiceProvider serviceProvider);
     }
 }
