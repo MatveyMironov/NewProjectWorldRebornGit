6eec371521bfe161ae905cb2f3a2ec926016d5a4
MatveyMironov <matveymironof@gmail.com>
2026-03-15 13:56:20 +0300

    Building Employer Manufactures Manager

---
+++ NewProjectWorldReborn/Assets/_Source/Building/Implementations/Employer Manufacture Building/Building Employer Manufactures Manager/BuildingEmployerManufacturesManagerSingleton.cs
@@ -0,0 +1,9 @@
+﻿namespace BuildingSystem.Implementations
+{
+    public static class BuildingEmployerManufacturesManagerSingleton
+    {
+        private static IBuildingEmployerManufacturesManager _instance;
+        public static IBuildingEmployerManufacturesManager Instance => 
+            _instance ??= new BuildingEmployerManufacturesManager(BuildingEmployersManagerSingleton.Instance, BuildingManufacturesManagerSingleton.Instance);
+    }
+}
--- NewProjectWorldReborn/Assets/_Source/Building/Implementations/Employer Manufacture Building/SingletonEmployerManufactureBuildingRegisterMB.cs
+++ NewProjectWorldReborn/Assets/_Source/Building/Implementations/Employer Manufacture Building/SingletonEmployerManufactureBuildingRegisterMB.cs
@@ -1,19 +1,13 @@
 using EmployerManufactureSystem;
-using EmployerSystem;
-using ManufactureSystem;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace BuildingSystem.Implementations
 {
     public class SingletonEmployerManufactureBuildingRegisterMB : MonoBehaviour
     {
-        private readonly Dictionary<Building, IEmployerManufacture> _buildings_employerManufactures = new();
-
-        private readonly IManufacturesManager _manufacturesManager = ManufacturesManagerSingleton.Instance;
-        private readonly IEmployersManager _employersManager = EmployersManagerSingleton.Instance;
-
         private readonly IStructureBuildingsManager _structureBuildingsManager = StructureBuildingsManagerSingleton.Instance;
+
+        private readonly IBuildingEmployerManufacturesManager _buildingEmployerManufacturesManager = BuildingEmployerManufacturesManagerSingleton.Instance;
 
         private void OnEnable()
         {
@@ -29,20 +23,12 @@ namespace BuildingSystem.Implementations
 
         private void RegisterBuilding(Building building, IEmployerManufacture employerManufacture)
         {
-            if (_buildings_employerManufactures.TryAdd(building, employerManufacture))
-            {
-                _manufacturesManager.TryAddManufacture(employerManufacture.Manufacture);
-                _employersManager.TryAddEmployer(employerManufacture.Employer);
-            }
+            _buildingEmployerManufacturesManager.TryAddBuildingEmployerManufacture(building, employerManufacture);
         }
 
         private void UnregisterBuilding(Building building)
         {
-            if (_buildings_employerManufactures.Remove(building, out IEmployerManufacture employerManufacture))
-            {
-                _manufacturesManager.TryRemoveManufacture(employerManufacture.Manufacture);
-                _employersManager.TryRemoveEmployer(employerManufacture.Employer);
-            }
+            _buildingEmployerManufacturesManager.TryRemoveBuildingEmployerManufacture(building);
         }
     }
 }
