cbf9e9c8bf0df3ebd63201c88982bbf70053c7f8
MatveyMironov <matveymironof@gmail.com>
2026-03-30 16:35:48 +0300

    Delete SingletonServiceBuildingRegisterMB.cs

--- NewProjectWorldReborn/Assets/_Source/Building/Implementations/Service Building/Service Building Register/SingletonServiceBuildingRegisterMB.cs
+++
@@ -1,8 +0,0 @@
-namespace BuildingSystem.Implementations
-{
-    public class SingletonServiceBuildingRegisterMB : AServiceBuildingRegisterMB
-    {
-        protected override IServiceBuildingsManager ServiceBuildingsManager { get; } = ServiceBuildingsManagerSingleton.Instance;
-        protected override IStructureBuildingsManager StructureBuildingsManager { get; } = StructureBuildingsManagerSingleton.Instance;
-    }
-}
