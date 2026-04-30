30abe8dccd17cf667f49dd0c3aa067f891c964f1
MatveyMironov <matveymironof@gmail.com>
2026-04-30 10:49:22 +0300

    Progression Game Events fixed

---
+++ NewProjectWorldReborn/Assets/_Source/Game Event/Single Argument/Implementations/Quest Chain/QuestChainGameEvent.cs
@@ -0,0 +1,8 @@
+using ProgressionSystem;
+using UnityEngine;
+
+namespace GameEventSystem.Implementations.ProgressionQuestChain
+{
+    [CreateAssetMenu(fileName = "New Quest Chain Event", menuName = "Event/Quest Chain")]
+    public class QuestChainGameEvent : AGameEvent<QuestChain> { }
+}
