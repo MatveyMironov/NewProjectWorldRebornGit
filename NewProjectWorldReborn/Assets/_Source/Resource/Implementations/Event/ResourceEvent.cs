b2ebcffdbe48b151f48c69089e826f61cb5e73ea
MatveyMironov <matveymironof@gmail.com>
2026-04-30 10:04:50 +0300

    Resource Event fixed

--- NewProjectWorldReborn/Assets/_Source/Resource/Implementations/Event/ResourceEvent.cs
+++
@@ -1,8 +0,0 @@
-using GameEventSystem;
-using UnityEngine;
-
-namespace ResourceSystem.Implementations
-{
-    [CreateAssetMenu(fileName = "New Resource Event", menuName = "Event/Resource")]
-    public class ResourceEvent : AGameEvent<IResourceDefinition> { }
-}
