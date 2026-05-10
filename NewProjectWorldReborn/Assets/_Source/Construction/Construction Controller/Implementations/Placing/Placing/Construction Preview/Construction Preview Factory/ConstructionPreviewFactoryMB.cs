6c8717617f3798382f7e0de23fcdb3bc0de5af38
MatveyMironov <matveymironof@gmail.com>
2026-05-10 11:35:19 +0300

    Refactoring

---
+++ NewProjectWorldReborn/Assets/_Source/Construction/Construction Controller/Implementations/Placing/Placing/Construction Preview/Construction Preview Factory/ConstructionPreviewFactoryMB.cs
@@ -0,0 +1,21 @@
+﻿using BuildingViewSystem;
+using UnityEngine;
+
+namespace PlacingSystem
+{
+    public class ConstructionPreviewFactoryMB : MonoBehaviour, IConstructionPreviewFactory
+    {
+        [SerializeField] private Transform constructionPreviewParent;
+        [SerializeField] private Grid grid;
+
+        [Header("Materials Switch")]
+        [SerializeField] private Material validPlacementMaterial;
+        [SerializeField] private Material invalidPlacementMaterial;
+
+        public ConstructionPreview CreateConstructionPreview(GameObject previewObject)
+        {
+            previewObject.transform.SetParent(constructionPreviewParent);
+            return new(previewObject, grid, validPlacementMaterial, invalidPlacementMaterial);
+        }
+    }
+}
