ba470d1d8988152676a1a55763f54a3f293607a7
MatveyMironov <matveymironof@gmail.com>
2026-05-11 20:20:28 +0300

    Construction Grid Manager SO

--- NewProjectWorldReborn/Assets/_Source/Construction/Construction Controller/Implementations/Placing/Placing/Invoke Placing Factory/InvokePlacingFactoryMB.cs
+++ NewProjectWorldReborn/Assets/_Source/Construction/Construction Controller/Implementations/Placing/Placing/Invoke Placing Factory/InvokePlacingFactoryMB.cs
@@ -11,7 +11,7 @@ namespace PlacingSystem
         [SerializeField] private ConstructionControllerMB constructionController;
 
         [Space]
-        [SerializeField] private ConstructionGridManagerMB constructionGridManager;
+        [SerializeField] private ConstructionGridManagerSO constructionGridManager;
         [SerializeField] private ConstructionPreviewFactoryMB constructionPreviewController;
         [SerializeField] private ACellsVisualizationMB occupiedCellsVisualization;
         [SerializeField] private BuildingStructureFactoryMB buildingViewCreator;
