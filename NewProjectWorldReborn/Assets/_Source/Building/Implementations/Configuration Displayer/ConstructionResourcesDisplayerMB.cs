d85ed256df01ee738aaea146e4f15fa17c488056
MatveyMironov <matveymironof@gmail.com>
2026-05-13 20:34:04 +0300

    UI

---
+++ NewProjectWorldReborn/Assets/_Source/Building/Implementations/Configuration Displayer/ConstructionResourcesDisplayerMB.cs
@@ -0,0 +1,60 @@
+using ResourceSystem;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BuildingSystem.Implementations
+{
+    public class ConstructionResourcesDisplayerMB : ABuildingConfigurationDisplayerMB
+    {
+        [SerializeField] private AResourceCountDisplayerMB resourceCountDisplayerPrefab;
+        [SerializeField] private Transform content;
+
+        private readonly List<AResourceCountDisplayerMB> _createdDisplayers = new();
+
+        public override void Clear()
+        {
+            int createdDisplayersCount = _createdDisplayers.Count;
+
+            for (int i = 0; i < createdDisplayersCount; i++)
+            {
+                _createdDisplayers.RemoveAt(0);
+            }
+        }
+
+        public override void DisplayBuildingConiguration(IBuildingConfiguration configuration)
+        {
+            Dictionary<IResourceDefinition, int> constructionResourcesDictionary = configuration.ConstructionResourcesDictionary;
+
+            if (constructionResourcesDictionary.Count <= 0)
+            {
+                Hide();
+                return;
+            }
+
+            Show();
+
+            foreach (var resourceCount in constructionResourcesDictionary)
+            {
+                IResourceDefinition resource = resourceCount.Key;
+                int count = resourceCount.Value;
+
+                AResourceCountDisplayerMB resourceCountDisplayer = Instantiate(resourceCountDisplayerPrefab, content);
+
+                resourceCountDisplayer.DisplayResource(resource);
+                resourceCountDisplayer.DisplayCount(count);
+
+                _createdDisplayers.Add(resourceCountDisplayer);
+            }
+        }
+
+        private void Hide()
+        {
+            gameObject.SetActive(false);
+        }
+
+        private void Show()
+        {
+            gameObject.SetActive(true);
+        }
+    }
+}
